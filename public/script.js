4a21d08e8af86a1d1d13943c4de82212913815f1
Arpit Raj <pkanojiya3@gmail.com>
2023-09-15 19:17:14 +0530

    added loading screen

---
+++ public/script.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  var welcomeScreen = document.querySelector('.welcome-screen');
+  
+  // Delay scrolling away the welcome screen by 2 seconds
+  setTimeout(function () {
+      welcomeScreen.style.transform = 'translateY(-100%)';
+      // Allow scrolling on the page
+      document.body.style.overflow = 'auto';
+  }, 2000); // 2000 milliseconds = 2 seconds
+});
