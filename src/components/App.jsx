ae7cb7dafbc7c195d9e7421f68462f3cfb1069f7
RAHUL SINGH  CHAUHAN <buky6180@gmail.com>
2023-09-16 12:55:14 +0530

    first change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Home from '../components/Home'; // Import your Home component
 
-import Navbar from "./Navbar";
-
-function App() {
-    return <>
-        <Navbar />
-    </>
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+    
+  );
+};
 
 export default App;
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Navbar";
+import "./index.css";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Home;
